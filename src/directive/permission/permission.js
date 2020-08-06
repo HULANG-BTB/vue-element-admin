837ff7ddc677d7c328011dba434ff32072a250e5
AloneH <admin@oibit.cn>
2020-08-06 13:57:35 +0800

    修改v-permission部分逻辑

--- src/directive/permission/permission.js
+++ src/directive/permission/permission.js
@@ -4,17 +4,14 @@ function checkPermission (el, binding) {
   const { value } = binding
   const roles = store.getters && store.getters.roles
 
-  if (value && value instanceof Array) {
-    if (value.length > 0) {
-      const permissionRoles = value
+  if (value && value instanceof Array && value.length > 0) {
+    const permissionRoles = value
 
-      const hasPermission = roles.some(role => {
-        return permissionRoles.includes(role)
-      })
-
-      if (!hasPermission) {
-        el.parentNode && el.parentNode.removeChild(el)
-      }
+    const hasPermission = roles.some(role => {
+      return permissionRoles.includes(role)
+    })
+    if (!hasPermission) {
+      el.parentNode && el.parentNode.removeChild(el)
     }
   } else {
     throw new Error(`need roles! Like v-permission="['admin','editor']"`)
