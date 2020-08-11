6cd2d42c8c6f6269a8012c1519a228e6ab8d545e
qiuheng <1969328206@qq.com>
2020-08-11 17:21:24 +0800

    qiu first commit

--- src/router/modules/user/user.js
+++ src/router/modules/user/user.js
@@ -1,41 +1,40 @@
 /* Layout */
 import Layout from '@/layout'
 
-export default [
-  {
-    path: '/user',
-    component: Layout,
-    redirect: '/user/user',
-    name: 'User',
-    meta: {
-      title: '用户管理',
-      icon: 'user'
-    },
-    children: [
-      {
-        path: 'user',
-        component: () => import('@/views/user/user'),
-        name: 'PageUser',
-        meta: { title: '用户管理' }
-      },
-      {
-        path: 'role',
-        component: () => import('@/views/user/role'),
-        name: 'PageRole',
-        meta: {
-          title: '角色管理',
-          roles: ['role_manager', 'admin']
-        }
-      },
-      {
-        path: 'permission',
-        component: () => import('@/views/user/permission'),
-        name: 'PagePermissions',
-        meta: {
-          title: '权限管理',
-          roles: ['permission_manager', 'admin']
-        }
+export default [{
+  path: '/user',
+  component: Layout,
+  redirect: '/user/user',
+  name: 'User',
+  meta: {
+    title: '用户管理',
+    icon: 'user'
+  },
+  children: [{
+      path: 'user',
+      component: () => import('@/views/user/user'),
+      name: 'PageUser',
+      meta: {
+        title: '用户管理'
       }
-    ]
-  }
-]
+    },
+    {
+      path: 'role',
+      component: () => import('@/views/user/role'),
+      name: 'PageRole',
+      meta: {
+        title: '角色管理',
+        roles: ['role_manager', 'admin']
+      }
+    },
+    {
+      path: 'permission',
+      component: () => import('@/views/user/permission'),
+      name: 'PagePermissions',
+      meta: {
+        title: '权限管理',
+        roles: ['permission_manager', 'admin']
+      }
+    }
+  ]
+}]
---
+++ src/views/qiuhengGroupViews/destroyApply.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="destroy-apply-container">
+    <el-header>
+      <el-form
+        :model="destroySearch"
+        label-width="100px"
+        label-position="labelPosition"
+      >
+        <el-row>
+          <el-col :span="7">
+            <el-form-item label="业务单号：">
+              <el-input
+                v-model="destroySearch.no"
+                placeholder="请输入需要查询的业务单号"
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :span="4">
+            <el-form-item label>
+              <el-button
+                icon="el-icon-search"
+                @click="handleSearch"
+              >搜索</el-button>
+            </el-form-item>
+          </el-col>
+          <el-col :span="4">
+            <el-button
+              type="primary"
+              @click="addDestroyApply()"
+            >新增</el-button>
+          </el-col>
+          <el-dialog
+            :visible.sync="dialogVisible"
+            :show-close="true"
+            width="80%"
+            top="6vh"
+            title="票据销毁申请——新增"
+          >
+            <add-destroy-apply-dialog></add-destroy-apply-dialog>
+          </el-dialog>
+        </el-row>
+      </el-form>
+
+    </el-header>
+
+    <el-pagination
+      background
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[10,100, 200, 300, 400]"
+      :page-size="10"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    >
+    </el-pagination>
+
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+      :default-sort="{prop: 'id'}"
+    >
+      <el-table-column
+        type="selection"
+        width="55"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="id"
+        label="序号"
+        width="50"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="status"
+        label="状态"
+        sortable
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="no"
+        label="业务单号"
+        sortable
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="编制日期"
+        sortable
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="type"
+        label="销毁类型"
+        sortable
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="changeMan"
+        label="经办人"
+        sortable
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="180"
+      >
+        <template slot-scope="scope">
+          <el-button
+            @click="handleClick(scope.row)"
+            type="text"
+            size="small"
+          >查看</el-button>
+          <el-button
+            @click="handleClick(scope.row)"
+            type="text"
+            size="small"
+          >修改</el-button>
+          <el-button
+            @click="handleClick(scope.row)"
+            type="text"
+            size="small"
+          >新增</el-button>
+          <el-button
+            type="text"
+            size="small"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import addDestroyApplyVue from './addDestroyApply';
+  export default {
+    components:{
+      "addDestroyApplyDialog": addDestroyApplyVue
+    },
+
+    methods: {
+      handleClick(row) {
+        console.log(row);
+      },
+      addDestroyApply(){
+        this.dialogVisible=true;
+      }
+    },
+
+    data() {
+      return {
+        tableData: [{
+          id: '1',
+          status: '未审核',
+          no: '90293092617',
+          date: '2020-08-10',
+          type: '核销票据销毁',
+          changeMan: '张三'
+        },{
+          id: '2',
+          status: '已审核',
+          no: '90293092618',
+          date: '2020-08-10',
+          type: '库存票据销毁',
+          changeMan: '李四'
+        },{
+          id: '3',
+          status: '已审核',
+          no: '90293092619',
+          date: '2020-08-10',
+          type: '核销票据销毁',
+          changeMan: '王二'
+        }],
+
+        dialogVisible: false,
+        labelPosition: "right",
+
+        destroySearch:{
+          no: ""
+        }
+      }
+    }
+  }
+</script>
