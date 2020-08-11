c47ce542bf88c35b985e457ff1080fed6fe6954a
Mr.lin <529042209@QQ.com>
2020-08-11 14:18:03 +0800

    归口部门完成

--- src/views/base-data/finDept/index.vue
+++ src/views/base-data/finDept/index.vue
@@ -1,51 +1,55 @@
 <template>
   <div class="content">
-    <el-form :inline="true" :model="queryData">
-      <el-form-item label="部门名称：">
+    <el-form ref="searchForm" :inline="true" :model="queryData">
+      <el-form-item label="部门名称：" prop="findeptName">
         <el-input
-          v-model="queryData.fFindeptName"
+          v-model="queryData.findeptName"
           placeholder="部门名称"
           size="small"
           style="width:200px"
         />
       </el-form-item>
 
-      <el-form-item label="是否启用：">
-        <el-select v-model="queryData.fIsEnable" placeholder="请选择部门状态 " size="small">
-          <el-option label="启用" value="1" />
-          <el-option label="停用" value="0" />
+      <el-form-item label="是否启用：" prop="isEnable">
+        <el-select v-model="queryData.isEnable" placeholder="请选择部门状态 " size="small">
+          <el-option label="启用" value="true" />
+          <el-option label="禁用" value="false" />
         </el-select>
       </el-form-item>
       <el-form-item>
         <el-button plain size="small" type="primary" @click="query">查询</el-button>
         <el-button plain size="small" type="primary" @click="handleAdd">新增</el-button>
+        <el-button plain size="small" type="primary" @click="clearSearchForm">清空</el-button>
       </el-form-item>
     </el-form>
     <!-- 弹出框 -->
     <el-dialog :title="formTitle" :visible.sync="dialogFormVisible" @close="handleClose">
       <el-form ref="popForm" :model="saveData" :rules="popForm.rules" label-width="120px">
-        <el-form-item label="部门名称" prop="fFindeptName">
-          <el-input v-model="saveData.fFindeptName" style="width:86% " />
+        <el-form-item label="部门名称" prop="findeptName">
+          <el-input v-model="saveData.findeptName" style="width:86% " />
         </el-form-item>
-        <el-form-item label="部门编码" prop="fFindeptCode">
-          <el-input v-model="saveData.fFindeptCode" style="width:86% " />
+        <el-form-item label="部门编码" prop="findeptCode">
+          <el-input v-model="saveData.findeptCode" style="width:86% " />
         </el-form-item>
-        <el-form-item label="区划编码" prop="fRgnCode">
+        <el-form-item label="区划编码" prop="rgnCodeArray">
           <el-cascader
-            v-model="saveData.fRgnCode"
+            v-model="saveData.rgnCodeArray"
             :options="cascader.data"
             :props="cascader.props"
-            :show-all-levels="false"
+            :show-all-levels="true"
           />
         </el-form-item>
-        <el-form-item label="联系人" prop="fLinkman">
-          <el-input v-model="saveData.fLinkman" style="width:86% " />
+        <el-form-item label="联系人" prop="linkman">
+          <el-input v-model="saveData.linkman" style="width:86% " />
         </el-form-item>
-        <el-form-item label="联系人电话" prop="fLinkTel">
-          <el-input v-model="saveData.fLinkTel" style="width:86% " />
+        <el-form-item label="联系人电话" prop="linkTel">
+          <el-input v-model="saveData.linkTel" style="width:86% " />
         </el-form-item>
-        <el-form-item label="是否启用" prop="fIsEnable">
-          <el-switch v-model="saveData.fIsEnable" />
+        <el-form-item label="地址" prop="addr">
+          <el-input v-model="saveData.addr" style="width:86% " />
+        </el-form-item>
+        <el-form-item label="是否启用" prop="isEnable">
+          <el-switch v-model="saveData.isEnable" />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -63,14 +67,15 @@
 
     <!-- 数据表格 -->
     <el-table :data="tableData" style="width: 100%">
-      <el-table-column label="部门编码" prop="fFindeptCode" />
-      <el-table-column label="部门名称" prop="fFindeptName" />
-      <el-table-column label="区划编码" prop="fRgnCode" />
-      <el-table-column label="联系人" prop="fLinkman" />
-      <el-table-column label="联系人电话" prop="fLinkTel" />
-      <el-table-column label="经办人" prop="fOperator" />
-      <el-table-column label="创建时间" prop="fCreateTime" />
-      <el-table-column :formatter="isEnable" label="是否启用" prop="fIsEnable" />
+      <el-table-column label="部门编码" prop="findeptCode" />
+      <el-table-column label="部门名称" prop="findeptName" />
+      <el-table-column label="区划编码" prop="rgnCode" />
+      <el-table-column label="联系人" prop="linkman" />
+      <el-table-column label="联系人电话" prop="linkTel" />
+      <el-table-column label="经办人" prop="operator" />
+      <el-table-column label="创建时间" prop="createTime" :formatter="dateFormat" />
+      <el-table-column label="地址" prop="addr" />
+      <el-table-column :formatter="isEnable" label="是否启用" prop="isEnable" />
       <el-table-column fixed="right" label="操作" width="150px">
         <template slot-scope="scope">
           <el-button size="small" type="text" @click="handleEdit(scope.row)">编辑</el-button>
@@ -80,11 +85,13 @@
     </el-table>
     <!-- 分页插件 -->
     <!-- total:数据总量  page-sizes页容量 current-page当前页 -->
-    <el-pagination :total="queryData.total" layout="prev, pager, next" />
+    <el-pagination :total="total" layout="prev, pager, next" />
   </div>
 </template>
 
 <script>
+import { queryFinDept, queryRngCode, saveFinDept, deleteById } from '@/api/finDept/finDept'
+
 export default {
   data () {
     return {
@@ -106,7 +113,7 @@ export default {
             name: '中央',
             parentId: 0,
             assortment: false,
-            children: []
+            children: null
           },
           {
             id: 2,
@@ -223,37 +230,39 @@ export default {
       },
       // 需要保存的数据
       saveData: {
-        fFindeptName: '',
-        fFindeptCode: '',
-        fLinkman: '',
-        fLinkTel: '',
-        fRgnCode: '',
-        fIsEnable: true
+        findeptName: '',
+        findeptCode: '',
+        linkTel: '',
+        linkman: '',
+        rgnCode: '',
+        rgnCodeArray: '',
+        isEnable: true,
+        addr: ''
       },
       // 查询条件数据
       queryData: {
         // 每页显示数量
-        pageCount: 10,
+        pageSize: 10,
         // 当前页
         pageNumber: 1,
         // 数据总数
         total: 50,
-        fFindeptName: '',
-        fIsEnable: null
+        findeptName: '',
+        isEnable: null
       },
       // 弹出框配置
       popForm: {
         rules: {
-          fFindeptName: [
+          findeptName: [
             { required: true, message: '请输入名称', trigger: 'blur' }
           ],
-          fFindeptCode: [
+          findeptCode: [
             { required: true, message: '请输入部门编码', trigger: 'change' }
           ],
-          fRgnCode: [
+          rgnCodeArray: [
             { required: true, message: '请选择区划编码', trigger: 'change' }
           ],
-          fLinkTel: [
+          linkTel: [
             {
               required: true,
               validator: (rule, value, callback) => {
@@ -276,7 +285,7 @@ export default {
               trigger: 'blur'
             }
           ],
-          fIsEnable: [{ required: true }]
+          isEnable: [{ required: true }]
         },
         // 内层嵌套的dialog
         innerVisible: false
@@ -284,8 +293,13 @@ export default {
       // 新增模态框是否弹出
       dialogFormVisible: false,
       // 添加，修改弹出框标题
-      formTitle: ''
+      formTitle: '',
+      total: 0
     }
+  },
+  mounted () {
+    this.query()
+    // this.getRngCode()
   },
   methods: {
     // msg弹框
@@ -307,6 +321,21 @@ export default {
     },
     // 查询条件接口
     query () {
+      queryFinDept(this.queryData).then(response => {
+        let list = []
+        list = response.data.list
+        for (let i = 0; i < list.length; i++) {
+          this.transferRgnArrayToCode(list[i])
+        }
+        this.tableData = list
+        this.total = response.data.total
+      })
+    },
+    transferRgnArrayToCode (item) {
+      let codeArray = []
+      codeArray = item.rgnCode.split(',')
+      item.rgnCodeArray = codeArray
+      item.rgnCode = codeArray[codeArray.length - 1]
     },
     // 编辑按钮操作
     handleEdit (row) {
@@ -321,50 +350,64 @@ export default {
     },
     // 删除
     del (row) {
+      deleteById(row.id).then(response => {
+        this.query()
+      })
     },
     // 保存
     submitForm (formName) {
+      const data = this.saveData
+
       this.$refs[formName]
         .validate()
         .then(() => {
-          this.submitData().then(() => {
-            this.dialogFormVisible = false
-            this.$message({
-              type: 'success',
-              message: '保存成功!'
-            })
+          saveFinDept(data).then((response) => {
+            if (response.success === true) {
+              this.dialogFormVisible = false
+              this.$message({
+                type: 'success',
+                message: '保存成功!'
+              })
+              this.query()
+            } else {
+              this.popForm.innerVisible = true
+            }
           })
         })
-        .catch(() => {
+        .catch((response) => {
           this.popForm.innerVisible = true
         })
     },
-    // 提交保存，修改数据
-    submitData () {
-      return new Promise(function (resolve, reject) {
-        // 发送请求
-        // 请求成功
-        if (true) {
-          resolve()
-        } else {
-          // 请求失败
-          reject()
-        }
-      })
-    },
     // 表格数据格式化
     isEnable (row, column, cellValue, index) {
-      console.log(cellValue)
+      if (cellValue === undefined) {
+        return null
+      }
       if (cellValue === true) {
         return '启用'
       } else {
         return '禁用'
       }
     },
+    dateFormat: function (row, column, cellValue, index) {
+      // row 表示一行数据, updateTime 表示要格式化的字段名称
+      var t = new Date(cellValue)
+      return t.getFullYear() + '-' + (t.getMonth() + 1) + '-' + t.getDate()
+    },
     // 对话框关闭事件
     handleClose () {
       this.$refs['popForm'].resetFields()
+    },
+    getRngCode () {
+      queryRngCode().then((response) => {
+        // this.cascader.data = response.query.data
+      })
+    },
+    clearSearchForm () {
+      this.$refs['searchForm'].resetFields()
+      this.query()
     }
+
   }
 }
 </script>
