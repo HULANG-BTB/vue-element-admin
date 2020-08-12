5f34294d0297ba4c9ad906e9ca16a1642302b96c
AloneH <admin@oibit.cn>
2020-08-12 11:08:26 +0800

    格式化代码

--- mock/permission.js
+++ mock/permission.js
@@ -1,63 +1,63 @@
 const permissionList = [
   {
-    "id": 1,
-    "name": "增加用户",
-    "url": "/user/save"
+    'id': 1,
+    'name': '增加用户',
+    'url': '/user/save'
   },
   {
-    "id": 2,
-    "name": "修改用户",
-    "url": "/user/update"
+    'id': 2,
+    'name': '修改用户',
+    'url': '/user/update'
   },
   {
-    "id": 3,
-    "name": "删除用户",
-    "url": "/user/remove"
+    'id': 3,
+    'name': '删除用户',
+    'url': '/user/remove'
   },
   {
-    "id": 4,
-    "name": "查询用户",
-    "url": "/user/list"
+    'id': 4,
+    'name': '查询用户',
+    'url': '/user/list'
   },
   {
-    "id": 5,
-    "name": "增加角色",
-    "url": "/role/add"
+    'id': 5,
+    'name': '增加角色',
+    'url': '/role/add'
   },
   {
-    "id": 6,
-    "name": "修改角色",
-    "url": "/role/update"
+    'id': 6,
+    'name': '修改角色',
+    'url': '/role/update'
   },
   {
-    "id": 7,
-    "name": "删除角色",
-    "url": "/role/remove"
+    'id': 7,
+    'name': '删除角色',
+    'url': '/role/remove'
   },
   {
-    "id": 8,
-    "name": "查询角色",
-    "url": "/role/list"
+    'id': 8,
+    'name': '查询角色',
+    'url': '/role/list'
   },
   {
-    "id": 9,
-    "name": "增加权限",
-    "url": "/permission/save"
+    'id': 9,
+    'name': '增加权限',
+    'url': '/permission/save'
   },
   {
-    "id": 10,
-    "name": "修改权限",
-    "url": "/permission/update"
+    'id': 10,
+    'name': '修改权限',
+    'url': '/permission/update'
   },
   {
-    "id": 11,
-    "name": "删除权限",
-    "url": "/permission/remov"
+    'id': 11,
+    'name': '删除权限',
+    'url': '/permission/remov'
   },
   {
-    "id": 12,
-    "name": "查询权限",
-    "url": "/permission/list"
+    'id': 12,
+    'name': '查询权限',
+    'url': '/permission/list'
   }
 ]
 
