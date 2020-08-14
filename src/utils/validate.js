9cca266208d7b4fbfd8284b0ddbd886738246532
AloneH <admin@oibit.cn>
2020-08-14 11:09:06 +0800

    添加日志日期校验规则

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -10,3 +10,27 @@ export function isExternal (path) {
   return /^(https?:|mailto:|tel:)/.test(path)
 }
 
+/**
+ * 验证是日期选择器
+ *
+ * @param {*} rule
+ * @param {*} value
+ * @param {*} callback
+ * @param {*} source
+ * @param {*} option
+ * @param {*} other
+ */
+export function validateDatePicker (rule, value, callback, source, option, other) {
+  const thisZero = new Date().setHours(0, 0, 0, 0)
+  const input = new Date(value).setHours(0, 0, 0, 0)
+  if (input < thisZero) {
+    callback(new Error('日期不能在今天之前'))
+  }
+  if (other) {
+    const otherDate = new Date(other).setHours(0, 0, 0, 0)
+    if (otherDate > input) {
+      callback(new Error('当前日期不能在开始日期之前'))
+    }
+  }
+  callback()
+}
