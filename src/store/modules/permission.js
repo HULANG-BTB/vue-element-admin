1baef6f772802776d2accb83481989626341f39c
AloneH <admin@oibit.cn>
2020-08-17 20:02:58 +0800

    删除console.log

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -6,7 +6,6 @@ import { asyncRoutes, constantRoutes } from '@/router'
  * @param route
  */
 function hasPermission (roles, route) {
-  console.log(route, roles)
   if (route.meta && route.meta.roles) {
     return roles.some(role => route.meta.roles.includes(role))
   } else {
