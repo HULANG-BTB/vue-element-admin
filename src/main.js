1ab28a839b6058d6e1bc4c032e442bbc5466da96
fly_tempester <fly_tempester@qq.com>
2020-08-20 17:37:48 +0800

    单位申领、财政审核的前端功能实现，仅风格未统一适配

--- src/main.js
+++ src/main.js
@@ -17,6 +17,8 @@ import '@/permission' // permission control
 
 import permission from '@/directive/permission/index.js'
 
+import moment from 'moment'
+
 Vue.use(permission)
 
 /**
@@ -38,6 +40,16 @@ Vue.use(ElementUI, { locale })
 // Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+Vue.filter('dateFmt', (input, formatString = "YYYY-MM-DD") => {
+  //es5函数参数设置默认值
+  //const lastFormatString = formatString || 
+
+  
+   // moment(input) 把时间字符串转成时间对象
+   // format(formatString) 把时间对象，按照指定格式，格式化成符合条件的字符串
+  return moment(input).format(formatString)
+})
 
 new Vue({
   el: '#app',
