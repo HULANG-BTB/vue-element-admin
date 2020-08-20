1450673386eac40f41656bf4f734ea47a7dc0ae1
chengwangze <chengwangze@163.com>
2020-08-20 17:43:41 +0800

    入库模块api中增加BASE_URL

--- src/api/stockIn.js
+++ src/api/stockIn.js
@@ -1,73 +1,84 @@
 import request from '@/utils/request'
 
-export function getStockList () {
-  return request({
-    url: '/stockIn/list',
-    method: 'get'
-  })
-}
+// 入库模块
 
+const BASE_URL = 'http://localhost:8080'
+
+// 分页查询入库单
 export function getStockListPage (page) {
   return request({
-    url: '/stockIn/listPage',
+    url: BASE_URL + '/stockIn/listPage',
     method: 'post',
     data: page
   })
 }
 
+// 生成业务单号
 export function getBillNumber () {
   return request({
-    url: '/stockIn/getId',
+    url: BASE_URL + '/stockIn/getId',
     method: 'get'
   })
 }
 
+// 新增入库单
 export function addStockIn (stockIn) {
   return request({
-    url: '/stockIn/add',
+    url: BASE_URL + '/stockIn/add',
     method: 'post',
     data: stockIn
   })
 }
 
+// 删除入库单
 export function deleteStockIn (removeDto) {
   return request({
-    url: '/stockIn/delete',
+    url: BASE_URL + '/stockIn/delete',
     method: 'post',
     data: removeDto
   })
 }
 
+// 根据id查询入库单详情
 export function getStockInInfo (id) {
   return request({
-    url: '/stockIn/getStockInInfo/' + id,
+    url: BASE_URL + '/stockIn/getStockInInfo/' + id,
     method: 'get'
   })
 }
 
+// 更新入库单
 export function updateStockIn (stockIn) {
   return request({
-    url: '/stockIn/update',
+    url: BASE_URL + '/stockIn/update',
     method: 'post',
     data: stockIn
   })
 }
 
+// 赋码
 export function getCodeFunction (getCode) {
+  // return request({
+  //   url: BASE_URL + '/create/single'
+  //   method: 'post',
+  //   data: getCode
+  // })
   return { 'beginCode': 2501, 'endCode': 3000 }
 }
 
+// 审核入库单
 export function change (stockChangeDTO) {
   return request({
-    url: '/stockIn/change',
+    url: BASE_URL + '/stockIn/change',
     method: 'post',
     data: stockChangeDTO
   })
 }
 
+// 入库
 export function store (deleteDto) {
   return request({
-    url: '/stockIn/store',
+    url: BASE_URL + '/stockIn/store',
     method: 'post',
     data: deleteDto
   })
