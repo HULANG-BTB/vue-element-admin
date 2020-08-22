68e0ad204a8be3a6f4685510cb502a5071b0ad10
zw <2439721180@qq.com>
2020-08-22 23:45:06 +0800

    申请退票完善

--- src/router/modules/stockreturn/stockreturn.js
+++ src/router/modules/stockreturn/stockreturn.js
@@ -9,20 +9,21 @@ export default [
     name: 'StockReturn',
     meta: {
       title: '申领退票',
-      icon: 'user'
+      icon: 'user',
+      role: ['financial_check', 'unit']
     },
     children: [
       {
         path: '/stockreturn1',
         component: () => import('@/views/stockreturn/stockreturn'),
         name: 'PageStockReturn1',
-        meta: { title: '票据申退' }
+        meta: { title: '票据申退', role: ['unit'] }
       },
       {
         path: '/stockreturn2',
         component: () => import('@/views/stockreturn/stockreturncheck'),
         name: 'PageStockReturn2',
-        meta: { title: '票据申退审核' }
+        meta: { title: '票据申退审核', role: ['financial_check'] }
       }
     ]
   }
