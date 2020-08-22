42925341d277e108f78a0c791953ff8188cba564
AloneH <admin@oibit.cn>
2020-08-22 19:50:34 +0800

    李大山组 添加接口前缀

--- src/api/payment.js
+++ src/api/payment.js
@@ -1,22 +1,25 @@
 import request from '@/utils/request'
+
+const BASE_URL = '/pay'
+
 export function payLogin (data) {
   return request({
-    url: '/pay/payLogin',
-    method: 'post',
+    url: BASE_URL + '/pay/payLogin',
+    method: BASE_URL + 'post',
     data
   })
 }
 
 export function getQrCode () {
   return request({
-    url: '/pay/getQrCode',
+    url: BASE_URL + '/pay/getQrCode',
     method: 'post'
   })
 }
 
 export function addAccIntoInfoDto (data) {
   return request({
-    url: '/pay/addAccIntoInfoDto',
+    url: BASE_URL + '/pay/addAccIntoInfoDto',
     method: 'post',
     data
   })
@@ -24,9 +27,8 @@ export function addAccIntoInfoDto (data) {
 
 export function getUUid (data) {
   return request({
-    url: '/pay/getUUid',
+    url: BASE_URL + '/pay/getUUid',
     method: 'post',
     data
   })
 }
-
