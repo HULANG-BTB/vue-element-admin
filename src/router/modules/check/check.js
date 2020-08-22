3bd4dfa3e32cd33fded3b3c1d7063b038ab2ea6e
dkhaos <e@qq.com>
2020-08-22 20:29:31 +0800

    msg

--- src/router/modules/check/check.js
+++ src/router/modules/check/check.js
@@ -2,20 +2,13 @@
 
 export default [
   {
-    path: '/check',
-    redirect: '/indexCheck',
-    name: 'Check',
-    meta: {
-      title: '主页查验',
-      icon: 'user'
-    }},
-
-  {
     path: '/indexCheck',
     component: () => import('@/views/msg/check/indexCheck'),
-    hidden: true,
     name: 'PageIndexCheck',
-    meta: { title: '主页查验' }
+    meta: {
+      title: '主页查验',
+      icon: 'el-icon-message'
+    }
   }
 
 ]
