23ad6e91461b5db003bc12c6fdd8dc41cb1af518
dkhaos <e@qq.com>
2020-08-24 08:56:52 +0800

    msg

--- src/views/msg/check/indexCheck.vue
+++ src/views/msg/check/indexCheck.vue
@@ -1,51 +1,13 @@
 <template>
   <div v-loading.body="loading" class="app-container">
-    <el-button type="text" class="login-button" icon="el-icon-user" @click="login">
+    <el-button
+      type="text"
+      class="login-button"
+      icon="el-icon-user"
+      @click="login"
+    >
       登录
     </el-button>
-<<<<<<< HEAD
-    <div class="box">
-      <el-card class="index-check" style="background-color: #3f5c6d2c;">
-        <el-image class="titleImg" :src="titleImg" />
-        <el-form :model="query" class="check-form" :rules="rules" :inline="true" @keyup.enter.native="handlerCheck">
-
-          <div v-if="requestType === 'tel'">
-            <el-form-item prop="tel">
-              <el-input v-model="query.tel" placeholder="请输入手机号码" clearable class="input-with-select" style="width: 350px;">
-                <el-select slot="prepend" v-model="requestType" class="el-select" placeholder="请选择" @change="selectChange">
-                  <el-option label="手机号码" value="tel" />
-                  <el-option label="票据号码" value="billId" />
-                </el-select>
-              </el-input>
-            </el-form-item>
-            <el-form-item prop="verifyCode">
-              <el-input v-model="query.verifyCode" placeholder="请输入校验码" clearable style="width: 350px;" class="input-with-select">
-                <template slot="prepend">校验码</template>
-              </el-input>
-            </el-form-item>
-          </div>
-
-          <div v-if="requestType === 'billId'">
-            <el-form-item prop="billId">
-              <el-input v-model="query.billId" placeholder="请输入票据号码" clearable class="input-with-select" style="width: 350px;">
-                <el-select slot="prepend" v-model="requestType" class="el-select" placeholder="请选择" @change="selectChange">
-                  <el-option label="手机号码" value="tel" />
-                  <el-option label="票据号码" value="billId" />
-                </el-select>
-              </el-input>
-            </el-form-item>
-            <el-form-item prop="verifyCode">
-              <el-input v-model="query.checkCode" placeholder="请输入校验码" clearable style="width: 350px;" class="input-with-select">
-                <template slot="prepend">校验码</template>
-              </el-input>
-            </el-form-item>
-          </div>
-
-          <el-form-item label>
-            <el-button class="check-button" type="primary" icon="el-icon-search" @click="handlerCheck">
-              查验
-            </el-button>
-=======
     <el-card class="index-check" style="width:400px;background-color: #3f5c6d2c;">
       <el-image :src="logo" class="logo" />
       <el-header
@@ -115,20 +77,20 @@
                 <el-option label="票据号" value="billId" />
               </el-select>
             </el-input>
->>>>>>> cfbcb54fa234dd7259eb0bec05a1461bc91a003f
           </el-form-item>
-        </el-form>
-      </el-card>
-    </div>
+          <el-form-item prop="verifyCode">
+            <el-input
+              v-model="query.checkCode"
+              placeholder="请输入校验码"
+              clearable
+              style="width: 350px;"
+              class="input-with-select"
+            >
+              <template slot="prepend">校验码</template>
+            </el-input>
+          </el-form-item>
+        </div>
 
-<<<<<<< HEAD
-    <el-dialog title="电子票据" :visible.sync="billDialogVisible">
-      <el-form :inline="true">
-        <el-table v-show="showTable" :stripe="true" :data="tableData">
-          <el-table-column prop="title" width="180" />
-          <el-table-column prop="property" />
-        </el-table>
-=======
         <el-form-item label>
           <el-button
             class="check-button"
@@ -138,16 +100,9 @@
             查验
           </el-button>
         </el-form-item>
->>>>>>> cfbcb54fa234dd7259eb0bec05a1461bc91a003f
       </el-form>
     </el-card>
 
-<<<<<<< HEAD
-      <el-button v-if="bill.fBillImgUrl!==null" type="button" style=" margin-top: 15px;" @click="openImg">查看票据详情</el-button>
-
-      <el-dialog :visible.sync="imgDialogVisible" title="电子票据" append-to-body>
-        <el-image v-if="bill.fBillImgUrl!==null" :src="bill.fBillImgUrl" />
-=======
     <el-dialog title="电子票据" :visible.sync="billDialogVisible">
       <el-form :inline="true">
         <el-table
@@ -176,7 +131,6 @@
           v-if="bill.fBillImgUrl!==null"
           :src="bill.fBillImgUrl"
         />
->>>>>>> cfbcb54fa234dd7259eb0bec05a1461bc91a003f
       </el-dialog>
     </el-dialog>
   </div>
@@ -201,85 +155,71 @@ export default {
         verifyCode: null
       },
       bill: {},
-      tableData: [
-        {
-          title: '开票时间',
-          property: ''
-        },
-        {
-          title: '票据类型',
-          property: ''
-        },
-        {
-          title: '票据号码',
-          property: ''
-        },
-        {
-          title: '票据代码',
-          property: ''
-        },
-        {
-          title: '校验位',
-          property: ''
-        },
-        {
-          title: '交款人',
-          property: ''
-        },
-        {
-          title: '合计金额',
-          property: ''
-        },
-        {
-          title: '开票单位',
-          property: ''
-        }
+      tableData: [{
+        title: '开票时间',
+        property: ''
+      }, {
+        title: '票据类型',
+        property: ''
+      }, {
+        title: '票据号码',
+        property: ''
+      }, {
+        title: '票据代码',
+        property: ''
+      }, {
+        title: '校验位',
+        property: ''
+      }, {
+        title: '交款人',
+        property: ''
+      }, {
+        title: '合计金额',
+        property: ''
+      }, {
+        title: '开票单位',
+        property: ''
+      }
       ],
       loading: false,
       showTable: false,
       rules: {
-        tel: [
-          {
-            required: true,
-            message: '请输入手机号码',
-            trigger: ['blur', 'change']
-          },
-          {
-            pattern: /^1\d{10}$/,
-            message: '无效手机号码'
-          }
+        tel: [{
+          required: true,
+          message: '请输入手机号码',
+          trigger: ['blur', 'change']
+        }, {
+          pattern: /^1\d{10}$/,
+          message: '无效手机号码'
+        }
         ],
-        verifyCode: [
-          {
-            required: true,
-            message: '请输入校验码',
-            trigger: ['blur', 'change']
-          },
-          {
-            pattern: /^[A-Za-z0-9]{6}$/,
-            message: '无效校验码'
-          }
+        verifyCode: [{
+          required: true,
+          message: '请输入校验码',
+          trigger: ['blur', 'change']
+        }, {
+          pattern: /^[A-Za-z0-9]{6}$/,
+          message: '无效校验码'
+        }
         ],
-        billId: [
-          {
-            required: true,
-            message: '请输入票据号码',
-            trigger: ['blur', 'change']
-          },
-          {
-            pattern: /^[A-Za-z0-9]{6}$/,
-            message: '无效票据号码'
-          }
+        billId: [{
+          required: true,
+          message: '请输入票据号码',
+          trigger: ['blur', 'change']
+        }, {
+          pattern: /^[A-Za-z0-9]{6}$/,
+          message: '无效票据号码'
+        }
         ]
       }
     }
   },
-  created () {},
+  created () {
+  },
   methods: {
     jump () {
       this.$router.push({
-        path: '/dashboard'
-      })
+        path: '/dashboard' })
     },
     clearTable () {
       this.showTable = false
@@ -306,28 +246,20 @@ export default {
       this.clearTable()
       this.bill = {}
       if (this.requestType === 'tel') {
-        await getBill(this.query)
-          .then((res) => {
-            this.billDialogVisible = true
-            this.bill = JSON.parse(res.data)
-            this.loadTable()
-            this.loading = false
-          })
-          .catch(() => {
-            this.loading = false
-          })
+        await getBill(this.query).then(res => {
+          this.billDialogVisible = true
+          this.bill = JSON.parse(res.data)
+          this.loadTable()
+          this.loading = false
+        }).catch(() => { this.loading = false })
       } else {
-        await billCheck(this.query)
-          .then((res) => {
-            this.billDialogVisible = true
-            this.bill = JSON.parse(res.data)
-            this.loading = false
-            this.loadTable()
-            this.loading = false
-          })
-          .catch(() => {
-            this.loading = false
-          })
+        await billCheck(this.query).then(res => {
+          this.billDialogVisible = true
+          this.bill = JSON.parse(res.data)
+          this.loading = false
+          this.loadTable()
+          this.loading = false
+        }).catch(() => { this.loading = false })
       }
       this.loading = false
     },
@@ -341,8 +273,7 @@ export default {
     },
     login () {
       this.$router.push({
-        path: '/login'
-      })
+        path: '/login' })
     },
     // 参数校验失败弹窗
     openErrormsg (msg) {
@@ -366,42 +297,22 @@ export default {
 </script>
 <style scoped lang="scss">
 .app-container {
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: -10;
-  zoom: 1;
-  background-color: #fff;
-  background: url("~@/assets/bg/bg.jpg");
-  background-size: cover;
-  -webkit-background-size: cover;
-  -o-background-size: cover;
-  background-position: left bottom;
-  text-align: center;
+    bottom: 0;
+    left: 0;
+    width:100%;
+    height:100%;
+    z-index:-10;
+    zoom: 1;
+    background-color: #fff;
+    background: url('~@/assets/bg/bg.jpg');
+    background-size: cover;
+    -webkit-background-size: cover;
+    -o-background-size: cover;
+    background-position: left  bottom;
+    text-align:center;
 
-  .box {
-    display: flex;
-    display: -webkit-flex;
-    align-items: center;
-    justify-content: center;
-    height: 95vh;
   }
-
   .index-check {
-<<<<<<< HEAD
-    width: 440px;
-  }
-
-  .input-with-select {
-    float: left;
-  }
-
-  .titleImg {
-    top: 10px;
-    left: 100px;
-    position: absolute;
-=======
     margin: 0 auto;
     margin-top: 12%;
   }
@@ -409,13 +320,11 @@ export default {
     color: #fff;
     background-color: #3f5c6d2c;
     width: 350px;
->>>>>>> cfbcb54fa234dd7259eb0bec05a1461bc91a003f
   }
 
   .check-form {
     margin-top: 15%;
   }
-
   .login-button {
     float: right;
     color: #fff;
@@ -427,11 +336,6 @@ export default {
     font-weight: lighter;
   }
 
-<<<<<<< HEAD
-  ::v-deep .el-select .el-input input {
-    width: 110px;
-    background-color: #fff;
-=======
   ::v-deep .el-select .el-input {
     width: 90px;
     color:#FFF
@@ -444,9 +348,7 @@ export default {
   ::v-deep .el-card__body  .el-input__inner{
     background-color: #6779832c;
     color: #FFF
->>>>>>> cfbcb54fa234dd7259eb0bec05a1461bc91a003f
   }
-
   ::v-deep .input-with-select .el-input-group__prepend {
     width: 90px;
     background-color: #3f5c6d2c;
@@ -457,14 +359,6 @@ export default {
     background-color: #3f5c6d2c;
   }
 
-<<<<<<< HEAD
-  ::v-deep .input-with-select .el-scrollbar__view {
-    background-color: #fff;
-  }
-
-  ::v-deep .el-form-item__error {
-    float: right;
-=======
   // logo
   ::v-deep .logo .el-image__inner {
     width: auto;
@@ -474,10 +368,8 @@ export default {
   }
   // 校验文字信息
   ::v-deep .el-form-item__error{
->>>>>>> cfbcb54fa234dd7259eb0bec05a1461bc91a003f
     font-size: 15px;
     font-weight: 800;
     margin-left: 100px;
   }
-}
 </style>
