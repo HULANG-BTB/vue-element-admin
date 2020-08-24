616520b0c9eca027d654e603cd9588e9f8c91d85
Dododoo <752093374@qq.com>
2020-08-24 17:43:52 +0800

    提交

--- src/api/qiuhengGroupApi/billInvoicing/bill.js
+++ src/api/qiuhengGroupApi/billInvoicing/bill.js
@@ -1,7 +1,8 @@
 import request from '@/utils/request'
 
-const BASE_URL = 'bill-invocing'
+const BASE_URL = 'http://localhost:9123'
 
+// 获取单位开票记录
 export function getBillListByPage (query) {
   return request({
     url: BASE_URL + '/billInvoicing/getPage',
@@ -9,6 +10,7 @@ export function getBillListByPage (query) {
     params: query
   })
 }
+// 查询单张票据
 export function getOneBill (billId) {
   return request({
     url: BASE_URL + '/billInvoicing/getUneCbillById',
@@ -16,6 +18,7 @@ export function getOneBill (billId) {
     params: { billId }
   })
 }
+// 新增票据前验证
 export function addBillVerify (unitName) {
   return request({
     url: BASE_URL + '/billInvoicing/addVerify',
@@ -31,6 +34,7 @@ export function addBill (data) {
     data
   })
 }
+// 获取通过单位端审核通过的票据信息
 export function getPassBillList (query) {
   return request({
     url: BASE_URL + '/billInvoicing/getPassBill',
@@ -38,6 +42,7 @@ export function getPassBillList (query) {
     params: query
   })
 }
+// 获取模板图片的url
 export function getImageUrl (billId, billNo) {
   return request({
     url: BASE_URL + '/billInvoicing/getImgUrl',
@@ -45,6 +50,7 @@ export function getImageUrl (billId, billNo) {
     params: { billId, billNo }
   })
 }
+// 获取盖章模板
 export function pdfSign (billId, billNo) {
   return request({
     url: BASE_URL + '/verify/sign',
@@ -52,9 +58,42 @@ export function pdfSign (billId, billNo) {
     params: { billId, billNo }
   })
 }
-export function getBill () {
+// 获取可用票据信息
+export function getAvaBill () {
   return request({
-    url: BASE_URL + '/billInvoicing/getBillInfo',
+    url: BASE_URL + '/billInvoicing/getAvaBill',
     method: 'get'
+  })
+}
+// 获取收费项目
+export function getItemInfo (unitName) {
+  return request({
+    url: BASE_URL + '/billInvoicing/getItemInfo',
+    method: 'get',
+    params: unitName
+  })
+}
+// 发邮件
+export function sendMail (billNo) {
+  return request({
+    url: BASE_URL + '/billInvoicing/sendMail',
+    method: 'get',
+    params: { billNo }
+  })
+}
+// 发短信
+export function sendSms (billNo) {
+  return request({
+    url: BASE_URL + '/billInvoicing/sendSms',
+    method: 'get',
+    params: { billNo }
+  })
+}
+// 修改票据目前的状态
+export function updateState (billNo, state) {
+  return request({
+    url: BASE_URL + '/billInvoicing/update',
+    method: 'get',
+    params: { billNo, state }
   })
 }
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -49,7 +49,6 @@ export default {
     }
   },
   mounted () {
-    this.getPublicKey()
   },
   methods: {
     handleClickOutside () {
