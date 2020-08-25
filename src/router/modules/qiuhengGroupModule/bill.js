7f683536f516a7f46bc523fbe636cc31b7aff3ca
qiuheng <1969328206@qq.com>
2020-08-25 21:23:31 +0800

    a

--- src/router/modules/qiuhengGroupModule/bill.js
+++ src/router/modules/qiuhengGroupModule/bill.js
@@ -11,15 +11,18 @@ export default [
     name: 'voicingbill',
     meta: {
       title: '票据管理',
-      icon: 'el-icon-s-ticket'
+      icon: 'el-icon-s-ticket',
+      roles: ['unit', 'financial']
     },
+    alwaysShow: true,
     children: [
       {
         path: 'voicingbill',
         component: () => import('@/views/qiuhengGroupViews/bill-invoicing/Home.vue'),
         name: 'voicingbill',
         meta: {
-          title: '开票'
+          title: '开票',
+          roles: ['unit']
           // roles: ['user_manager', 'editor']
         }
       },
@@ -27,7 +30,9 @@ export default [
         path: 'verify',
         component: () => import('@/views/qiuhengGroupViews/verify/Verify.vue'),
         name: 'verify',
-        meta: { title: '开票审验'
+        meta: {
+          title: '开票审验',
+          roles: ['unit']
           // roles: ['user_manager', 'editor']
         }
       },
@@ -36,7 +41,8 @@ export default [
         component: () => import('@/views/qiuhengGroupViews/bill-invoicing/Ticket.vue'),
         name: 'ticket',
         meta: {
-          title: '新增开票'
+          title: '新增开票',
+          roles: ['unit']
         },
         hidden: true
       },
@@ -47,7 +53,7 @@ export default [
         props: true,
         meta: {
           title: '缴款书',
-          roles: []
+          roles: ['unit']
         },
         hidden: true
       }
