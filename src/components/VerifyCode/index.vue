78c3edc98a274c2ff2232e794344bc08a3f59872
AloneH <admin@oibit.cn>
2020-08-25 17:43:53 +0800

    验证码

---
+++ src/components/VerifyCode/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>123</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    /** 生成一个随机数**/
+    randomNum (min, max) {
+      return Math.floor(Math.random() * (max - min) + min)
+    },
+
+    /** 生成一个随机色**/
+    randomColor (min, max) {
+      var r = this.randomNum(min, max)
+      var g = this.randomNum(min, max)
+      var b = this.randomNum(min, max)
+      return 'rgb(' + r + ',' + g + ',' + b + ')'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
