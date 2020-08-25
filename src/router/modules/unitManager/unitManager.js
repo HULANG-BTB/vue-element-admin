b362c8a00acc726415851f36efcb712081d7b6c4
wzh777 <15860920528@163.com>
2020-08-25 20:04:42 +0800

    修改路由权限控制

--- src/router/modules/unitManager/unitManager.js
+++ src/router/modules/unitManager/unitManager.js
@@ -12,7 +12,7 @@ const unitManager = {
       path: 'unitManager',
       component: () => import('@/views/unitManager/unitManager'),
       name: 'unitManager',
-      meta: { title: '单位管理', icon: 'icon' }
+      meta: { title: '单位管理', roles: ['financial', 'admin', 'financial_check'] }
     },
     {
       path: 'unitManagerDetail/:id',
@@ -21,68 +21,69 @@ const unitManager = {
       props: (route) => ({
         id: route.params.id
       }),
-      meta: { title: '单位管理详情(财政端)', icon: 'table' }
+      hidden: true,
+      meta: { title: '单位管理详情(财政端)' }
     },
     {
       path: 'unitManagerDetailUnit',
       component: () => import('@/views/unitManager/unitManagerDetailUnit'),
       name: 'unitManagerDetailUnit',
-      meta: { title: '单位管理详情(单位端)', icon: 'table' }
+      meta: { title: '单位信息', roles: ['unit'] }
     },
     {
       path: 'unitCheck',
       component: () => import('@/views/unitManager/unitCheck'),
       name: 'unitCheck',
-      meta: { title: '单位审核', icon: 'icon' }
+      meta: { title: '单位审核', roles: ['financial_check', 'admin'] }
     },
     {
       path: 'adminDepart',
       component: () => import('@/views/unitManager/adminDepart'),
       name: 'adminDepart',
-      meta: { title: '主管部门管理', icon: 'link' }
+      meta: { title: '主管部门管理', roles: ['financial', 'financial_check', 'admin'] }
     },
     {
       path: 'purchLicense',
       component: () => import('@/views/unitManager/purchLicense'),
       name: 'purchLicense',
-      meta: { title: '准购证管理', icon: 'edit' }
+      meta: { title: '准购证管理', roles: ['financial', 'financial_check', 'admin'] }
     },
     {
       path: 'purchLicenseCheck',
       component: () => import('@/views/unitManager/purchLicenseCheck'),
       name: 'purchLicenseCheck',
-      meta: { title: '准购证审核', icon: 'link' }
+      meta: { title: '准购证审核', roles: ['financial_check', 'admin'] }
     },
     {
       path: 'unitPayer',
       component: () => import('@/views/unitManager/unitPayer'),
       name: 'unitPayer',
-      meta: { title: '缴款人管理', icon: 'icon' }
+      meta: { title: '缴款人管理', roles: ['unit'] }
     },
     {
       path: 'unitPlaceManager',
       component: () => import('@/views/unitManager/unitPlaceManager'),
       name: 'unitPlaceManager',
-      meta: { title: '开票点管理', icon: 'icon' }
+      meta: { title: '开票点管理', roles: ['unit'] }
     },
     {
       path: 'projectGroup',
       component: () => import('@/views/projectManager/projectGroup'),
       name: 'projectGroup',
-      meta: { title: '项目分组', icon: 'link' },
+      meta: { title: '分组管理', roles: ['unit'] },
       roles: ['admin']
     },
     {
       path: 'unitBillManager',
       component: () => import('@/views/unitManager/unitBillManager'),
       name: 'unitBillManager',
-      meta: { title: '可用票据', icon: 'edit' }
+      meta: { title: '可用票据', roles: ['unit'] }
     },
     {
       path: 'unitItemManager',
       component: () => import('@/views/unitManager/unitItemManager'),
       name: 'unitItemManager',
-      meta: { title: '可用项目', icon: 'edit' }
+      meta: { title: '可用项目', roles: ['unit'] }
     }
   ]
 }
